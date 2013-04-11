7cd5db84d5c5417ddba0507e277d64f0213b5cb0
bewebi <bewebi@earthlink.net>
2013-04-11 13:24:52 -0300

    comments and neatness

--- findReplace.py
+++ findReplace.py
@@ -1,5 +1,6 @@
 import re
 
+#opens the text file and returns it as one string
 def openText(txt):
     t = ""
     l = open(txt).readlines()
@@ -8,8 +9,9 @@ def openText(txt):
 
     return t
 
-txt = openText("emporer.txt")
-
+#the following finds every sentence with the word in question but gives quite a bit more than you need
+#params should have {'word': *word*, 'text' : *text to search*}
+#returns a list of the sentences that include *word*
 def find(params):
     w = params['word']
     t = params['text']
@@ -22,6 +24,8 @@ def find(params):
 
     #print w + " and " + t
 
+#testing:
+txt = openText("emperor.txt")
 p = {'word': "old", 'text': txt}
 
 find(p)
