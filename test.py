301fd18d1aade5d699f5176b36c7390ddc55054f
Eric Cerny <ericc6134@gmail.com>
2013-04-21 21:48:28 -0400

    finally acquired a translation api

---
+++ test.py
@@ -0,0 +1,6 @@
+from microsofttranslator import Translator
+
+fish = Translator("WWEB","JY8fd3buaL3ZfyXQJKgA/NU54sLpDYZsGUecIamwoPM=")
+
+print fish.translate("your", "es")
+print fish.translate("mom", "es")
