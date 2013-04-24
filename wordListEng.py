f797dffaeaf48035ff1df33c41f4e4f1503a8905
Eric Cerny <ericc6134@gmail.com>
2013-04-24 12:30:45 -0400

    word frequencies

--- wordListEng.py
+++ wordListEng.py
@@ -1,2 +1,17 @@
+import string
 
-print open("texts/lm.txt", "r").read()[0:2000]
+s = open("texts/lm.txt", "r").readlines()
+s = [ a.translate(string.maketrans("",""), string.punctuation).strip() for a in s if a != '']
+#print s
+words = [ a.split(" ") for a in s ]
+words = [ w for s in words for w in s ]
+#print words
+freqs = {}
+print freqs["hey"]
+def createWordList():
+    for w in words:
+        if word in freqs.keys():
+            freqs[word] = freqs[word] + 1
+        else:
+            freqs[word] = 1
+
