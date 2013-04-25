d49c3fec459f31a87c61b28f13531a3fc5cbb0fd
Eric Cerny <ericc6134@gmail.com>
2013-04-25 12:30:59 -0400

    freqs

--- wordListEng.py
+++ wordListEng.py
@@ -1,17 +1,18 @@
 import string
+from collections import Counter
+from time import time
 
 s = open("texts/lm.txt", "r").readlines()
-s = [ a.translate(string.maketrans("",""), string.punctuation).strip() for a in s if a != '']
+s = [ a.translate(string.maketrans("",""), string.punctuation).strip().upper() for a in s if a != '']
 #print s
 words = [ a.split(" ") for a in s ]
 words = [ w for s in words for w in s ]
-#print words
-freqs = {}
-print freqs["hey"]
-def createWordList():
-    for w in words:
-        if word in freqs.keys():
-            freqs[word] = freqs[word] + 1
-        else:
-            freqs[word] = 1
 
+
+def createWordList():
+    return Counter(words)
+
+start = time()
+ct =  createWordList()
+print type(ct)
+print time() - start
