4ad1606e0360919bdf5970bd6da0e15c11e6e299
Eric Cerny <ericc6134@gmail.com>
2013-04-29 02:08:17 -0400

    nltk woes

--- wordListEng.py
+++ wordListEng.py
@@ -1,18 +1,21 @@
 import string
 from collections import Counter
 from time import time
+import nltk
 
-s = open("texts/lm.txt", "r").readlines()
-s = [ a.translate(string.maketrans("",""), string.punctuation).strip().upper() for a in s if a != '']
+start = time()
+s = open("texts/lm.txt", "r").read().split(".")
+s = [ a.translate(string.maketrans("",""), string.punctuation).strip().lower() for a in s if a != '']
+print s[300]
+print nltk.pos_tag(s[300])
 #print s
-words = [ a.split(" ") for a in s ]
-words = [ w for s in words for w in s ]
+#words = [ a.split(" ") for a in s ]
+#words = [ w for s in words for w in s ]
 
 
 def createWordList():
     return Counter(words)
 
-start = time()
-ct =  createWordList()
-print type(ct)
+#ct =  createWordList()
+#print type(ct)
 print time() - start
