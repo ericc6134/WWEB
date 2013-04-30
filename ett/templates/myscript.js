1b2ac9701bf30d296aff71f568f887479c3b8281
allidoiswin <fallenpwr@gmail.com>
2013-04-30 12:34:53 -0400

    case insensitive, arrays of words to translate

--- ett/templates/myscript.js
+++ ett/templates/myscript.js
@@ -10,4 +10,15 @@ console.log("hello");
  */
 (function($){$.fn.replaceText=function(b,a,c){return this.each(function(){var f=this.firstChild,g,e,d=[];if(f){do{if(f.nodeType===3){g=f.nodeValue;e=g.replace(b,a);if(e!==g){if(!c&&/</.test(e)){$(f).before(e);d.push(f)}else{f.nodeValue=e}}}}while(f=f.nextSibling)}d.length&&$(d).remove()})}})(jQuery);
 
-$("body *").replaceText( /Password/gi, "the other" );
+var rt1 = [];
+var rt2 = [];
+rt1.push("password", "Comfirm");
+rt2.push("<b>the others</b>", "<b>real</b>");
+
+for(var i = 0; i < rt1.length; i++){
+
+	var re = new RegExp(rt1[i], "i");
+
+	$("body *").replaceText( re, rt2[i] );
+
+}
