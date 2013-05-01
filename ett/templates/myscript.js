c41f522266eecba071f1a5015a73c17e1e428f28
allidoiswin <fallenpwr@gmail.com>
2013-04-30 22:02:23 -0400

    can read .csv file and replace text based on it

--- ett/templates/myscript.js
+++ ett/templates/myscript.js
@@ -10,15 +10,66 @@ console.log("hello");
  */
 (function($){$.fn.replaceText=function(b,a,c){return this.each(function(){var f=this.firstChild,g,e,d=[];if(f){do{if(f.nodeType===3){g=f.nodeValue;e=g.replace(b,a);if(e!==g){if(!c&&/</.test(e)){$(f).before(e);d.push(f)}else{f.nodeValue=e}}}}while(f=f.nextSibling)}d.length&&$(d).remove()})}})(jQuery);
 
-var rt1 = [];
-var rt2 = [];
-rt1.push("password", "Comfirm");
-rt2.push("<b>the others</b>", "<b>real</b>");
+
+////// CSV Reader
+$(document).ready(function() {
+    $.ajax({
+        type: "GET",
+        url: chrome.extension.getURL("/dummy.csv"),
+        dataType: "text",
+        success: function(data) {processData(data);}
+     });
+});
+
+
+////// Processing CSV into array
+function processData(allText) {
+
+	console.log('initiate');
+	console.log(allText);
+    var record_num = 2;  // or however many elements there are in each row
+    var allTextLines = allText.split(/\r\n|\n/);
+	console.log(allTextLines);
+
+   // var entries = allTextLines[0].split(',');
+	//console.log(entries);
+
+	var entries = [];
+
+	for(var i = 0; i < allTextLines.length - 1; i++){
+		entries.push(allTextLines[i].split(','));
+		console.log(entries[i]);
+	}
+	console.log(entries);
+
+////////// Processing "entries" into 2 arrays (rt1 = before, rt2 = after)
+	
+	var rt1 = [];
+	var rt2 = [];
+	for(var i = 0; i < entries.length; i++){
+		rt1.push(entries[i][0]);
+		rt2.push(entries[i][1]);
+	}
+
+
+/////// Replace
 
 for(var i = 0; i < rt1.length; i++){
 
 	var re = new RegExp(rt1[i], "i");
 
-	$("body *").replaceText( re, rt2[i] );
+	$("body *").replaceText( re, "<b>" + rt2[i] + "</b>" );
+	$("body *").replaceText( "\\F1" , "ñ");
 
 }
+
+
+
+
+
+
+
+}
+
+
+
