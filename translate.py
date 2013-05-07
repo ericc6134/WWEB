eb048d747f871284f69b2f8e9551c76043301824
Eric Cerny <ericc6134@gmail.com>
2013-05-06 22:02:38 -0400

    translator about to run

--- translate.py
+++ translate.py
@@ -1,8 +1,12 @@
-import microsofttranslator as mt
+from microsofttranslator import Translator
 import csv
 
-t = mt.Translator("WWEB","ml7-win-wilson-eric-bernie")
+t = Translator("WWEB","ml7-win-wilson-eric-bernie")
 #            ClientID,Client Secret
 
 words = open("words.csv").readlines()
-print words[10]
+words = [ word.strip() for word in words ]
+with open("engToSpan.csv","w") as csvfile:
+    writer = csv.writer(csvfile, delimiter="\n")
+    for w in words:
+        writer.writerow( [w + "," + t.translate(w,"es")] )
